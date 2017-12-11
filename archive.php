dc0d7fbc913592cb06585fb3a92e910aa066237f
Justin Peacock <justin@sixteenbit.com>
2017-12-11 17:33:24 -0500

    more formatting

--- archive.php
+++ archive.php
@@ -6,43 +6,43 @@
  */
 
 get_header(); ?>
-			
-	<div class="content">
-	
-		<div class="inner-content grid-x grid-margin-x grid-padding-x">
-		
-			<main class="main small-12 medium-8 large-8 cell" role="main">
-			
-					<header>
-					<h1 class="page-title"><?php the_archive_title(); ?></h1>
-					<?php the_archive_description( '<div class="taxonomy-description">', '</div>' ); ?>
-				</header>
-		
-				<?php
-				if ( have_posts() ) :
-					while ( have_posts() ) :
-						the_post();
-?>
-			 
+
+<div class="content">
+
+	<div class="inner-content grid-x grid-margin-x grid-padding-x">
+
+		<main class="main small-12 medium-8 large-8 cell" role="main">
+
+			<header>
+				<h1 class="page-title"><?php the_archive_title(); ?></h1>
+				<?php the_archive_description( '<div class="taxonomy-description">', '</div>' ); ?>
+			</header>
+
+			<?php
+			if ( have_posts() ) :
+				while ( have_posts() ) :
+					the_post();
+					?>
+
 					<!-- To see additional archive styles, visit the /parts directory -->
 					<?php get_template_part( 'parts/loop', 'archive' ); ?>
-				
-					<?php endwhile; ?>	
 
-					<?php joints_page_navi(); ?>
-					
-				<?php else : ?>
-											
-					<?php get_template_part( 'parts/content', 'missing' ); ?>
-						
-				<?php endif; ?>
-		
-			</main> <!-- end #main -->
-	
-			<?php get_sidebar(); ?>
-	
-			</div> <!-- end #inner-content -->
-	
-		</div> <!-- end #content -->
+				<?php endwhile; ?>
+
+				<?php joints_page_navi(); ?>
+
+			<?php else : ?>
+
+				<?php get_template_part( 'parts/content', 'missing' ); ?>
+
+			<?php endif; ?>
+
+		</main> <!-- end #main -->
+
+		<?php get_sidebar(); ?>
+
+	</div> <!-- end #inner-content -->
+
+</div> <!-- end #content -->
 
 <?php get_footer(); ?>
--- parts/content-byline.php
+++ parts/content-byline.php
@@ -4,6 +4,4 @@
  */
 ?>
 
-<p class="byline">
-	Posted on <?php the_time( 'F j, Y' ); ?> by <?php the_author_posts_link(); ?>  - <?php the_category( ', ' ); ?>
-</p>	
+<p class="byline">Posted on <?php the_time( 'F j, Y' ); ?> by <?php the_author_posts_link(); ?>  - <?php the_category( ', ' ); ?></p>
